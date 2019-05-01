fee1ca04890df7393eced6a5270d7aaab41c72b7
jones <jones@koji-projects.com>
2019-05-01 19:26:35 +0000

    more logging

--- setup.js
+++ setup.js
@@ -1,6 +1,7 @@
 var buildConfig = require('./tools/buildConfig.js');
 
 module.exports = () => {
+    console.log('koji-tools setup');
     const chokidar = require('chokidar');
     refresh();
  
@@ -16,5 +17,6 @@ function refresh() {
     var config = JSON.stringify({ config: JSON.parse(buildConfig()) });
     fs.writeFile(`${__dirname}/config.json`, config, (err) => {
         if (err) console.log(err);
+        console.log('new config');
     });
 }
