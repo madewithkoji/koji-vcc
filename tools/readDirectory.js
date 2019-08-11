e0fe9cb1089d154f8bff3d568e6b0ce7b08eb36a
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-08-11 23:44:43 +0430

    perf(readDirectory): Count only git-indexed files instead of all files (including node_modules, ...)
    Improving the performance through including only git-indexed files. As a result, some unnecessary
    paths (".git" and all 'gitignored' paths like "node_modules" and IDE-specific directories) will be
    excluded. The difference is significant in large (and even medium) projects with lots of
    dependencies.
    
    See: https://github.com/madewithkoji/koji-tools/issues/10

--- tools/readDirectory.js
+++ tools/readDirectory.js
@@ -1,21 +1,18 @@
-const fs = require('fs');
+const {execSync} = require('child_process')
+const {_YLW, RED, RST} = require('ansi-colors-and-styles')
+const path = require('path')
 
-// Recurse through all directories to find koji dotfiles
-module.exports = (directory) => readDirectory(directory);
-
-
-function readDirectory(directory) {
-  let results = [];
-  fs
-    .readdirSync(directory)
-    .forEach((fileName) => {
-      const file = `${directory}/${fileName}`;
-      const stat = fs.statSync(file);
-      if (stat && stat.isDirectory()) {
-        results = results.concat(readDirectory(file));
-      } else {
-        results.push(file);
-      }
-    });
-  return results;
+// Get all git-indexed paths to find koji files
+module.exports = directory => {
+   try {
+      return execSync('git ls-files', {cwd: directory}).toString()
+            .replace(/\n$/, '')
+            .split('\n')
+            .map(relativePath => path.resolve(directory, relativePath))
+   } catch (error) {
+      throw Error(error.message +
+            `${_YLW}${RED}Have you installed "git" and added it to the "PATH"? If no see: ` +
+            `https://git-scm.com/book/en/v2/Getting-Started-Installing-Git${RST}\n`
+      )
+   }
 }
