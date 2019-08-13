de53ef15cb03ef628e9f145a76301dd60dadb092
Svarog1389 <admin@koji-projects.com>
2019-08-12 22:01:26 -0700

    Update

--- tools/wrapConsole.js
+++ tools/wrapConsole.js
@@ -28,62 +28,66 @@ module.exports = () => {
     error: console.error.bind(console),
   };
 
-  const consoleOverrides = {
-    ...console,
-    log: (...args) => {
-      window.__originalConsole.log.apply(this, args);
-      if (window.parent) {
+  try {
+    const consoleOverrides = {
+      ...console,
+      log: (...args) => {
+        window.__originalConsole.log.apply(this, args);
+        if (window.parent) {
+            try {
+              window.parent.postMessage({
+                  action: 'log',
+                  payload: { args },
+              }, '*');
+            } catch (err) {
+                //
+            }
+        }
+      },
+      info: (...args) => {
+        window.__originalConsole.info.apply(this, args);
+        if (window.parent) {
           try {
-            window.parent.postMessage({
-                action: 'log',
-                payload: { args },
-            }, '*');
-          } catch (err) {
-              //
-          }
-      }
-    },
-    info: (...args) => {
-      window.__originalConsole.info.apply(this, args);
-      if (window.parent) {
-        try {
-            window.parent.postMessage({
-                action: 'info',
-                payload: { args },
-            }, '*');
-          } catch (err) {
-              //
-          }
-      }
-    },
-    warn: (...args) => {
-      window.__originalConsole.warn.apply(this, args);
-      if (window.parent) {
-        try {
-            window.parent.postMessage({
-                action: 'warn',
-                payload: { args },
-            }, '*');
-          } catch (err) {
-              //
-          }
-      }
-    },
-    error: (...args) => {
-      window.__originalConsole.error.apply(this, args);
-      if (window.parent) {
+              window.parent.postMessage({
+                  action: 'info',
+                  payload: { args },
+              }, '*');
+            } catch (err) {
+                //
+            }
+        }
+      },
+      warn: (...args) => {
+        window.__originalConsole.warn.apply(this, args);
+        if (window.parent) {
           try {
-            window.parent.postMessage({
-                action: 'error',
-                payload: { args },
-            }, '*');
-          } catch (err) {
-              //
-          }
-      }
-    },
-  };
-  console = consoleOverrides;
+              window.parent.postMessage({
+                  action: 'warn',
+                  payload: { args },
+              }, '*');
+            } catch (err) {
+                //
+            }
+        }
+      },
+      error: (...args) => {
+        window.__originalConsole.error.apply(this, args);
+        if (window.parent) {
+            try {
+              window.parent.postMessage({
+                  action: 'error',
+                  payload: { args },
+              }, '*');
+            } catch (err) {
+                //
+            }
+        }
+      },
+    };
+    console = consoleOverrides;
+  } catch (err) {
+    //
+  }
 
   // Wrap error
   window.onerror = (message, source, lineNumber) => {
