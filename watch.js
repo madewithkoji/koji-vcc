ce305f8e7ccded099dbc839e4baa3a8b67ebd469
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-08-22 03:04:16 +0430

    refactor(watch.js): Using "chokidar" dependency instead of node's built-in way (`fs.watch()`)
    close #18

--- watch.js
+++ watch.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const path = require('path')
+const chokidar = require('chokidar')
 const readDirectory = require('./tools/readDirectory.js')
 const findRootDirectory = require('./tools/findRootDirectory.js')
 const refresh = require('./refresh.js')
@@ -13,7 +14,7 @@ class EventBuffering {
 	reset() {
 		/**
 		 * Buffered events
-		 * @type {{event: string, fileName: string}[]}
+		 * @type {{event: string, path: string}[]}
 		 */
 		this.events = []
 		
@@ -36,58 +37,61 @@ const eventBuffering = new EventBuffering()
 
 module.exports = () => {
 	console.log('koji-tools watching')
-	// const props = JSON.parse(refresh());
+	// const props = JSON.parse(refresh())
 	// output what the server wants us to in order to start the preview window
-	// console.log(props.config.develop.frontend.events.built);
+	// console.log(props.config.develop.frontend.events.built)
 	// NOTE: figure out what to do about this one, because we cant output this before the server is ready...
 	
 	// make sure that its in there to start, postinstall has been doing so weird stuff
 	refresh()
 	// watch the .koji directory from a node_modules directory...
-	let root = findRootDirectory()
-	readDirectory(root)
+	const files = readDirectory(findRootDirectory())
 			.filter(path => (path.endsWith('koji.json') || path.includes('.koji')) && !path.includes('.koji-resources'))
-			.forEach(path => {
-				fs.access(path, fs.R_OK, err => {
-					if (err) {
-						console.error(`Access denied: "${path}".\n${err}`)
-						return
-					}
-					console.log('Watching', path)
-					
-					fs.watch(path, (event, fileName) => {
-						const newEvent = {event: event, fileName: fileName}
-						const now = Date.now()
-						
-						if (now > eventBuffering.flushTime) {
-							// Don't handle subsequent events until MAX_DELAY from now
-							eventBuffering.flushTime = now + EventBuffering.MAX_DELAY
-							handleEvents(newEvent)
-							return
-						}
-						
-						eventBuffering.events.push(newEvent)
-						
-						if (eventBuffering.timeoutID === 0)  // If a timer hasn't already been set
-							eventBuffering.timeoutID = setTimeout(() => {
-								let bufferedEventsCopy
-								
-								/*synchronized(eventBuffering.events)*/
-								{
-									// This block must be synchronized. But not needed to be explicit in Node.js! See https://stackoverflow.com/a/9129149/5318303
-									bufferedEventsCopy = eventBuffering.events
-									eventBuffering.reset()
-								}
-								
-								handleEvents(...bufferedEventsCopy)
-							}, eventBuffering.flushTime - now /* => wait until flushTime */)
-					})
-				})
+	
+	// Initialize watcher.
+	const watcher = chokidar.watch(files)
+
+	// Add event listeners.
+	watcher
+			.on('error', error => console.error(`Watcher error: ${error}`))
+			.on('change', eventManager)
+			.on('ready', () => {
+				for (const [dir, files] of Object.entries(watcher.getWatched()))
+					for (const file of files)
+						console.log('Watching', path.join(dir, file))
 			})
 }
 
+function eventManager(path) {
+	const newEvent = {event: 'change', path: path}
+	const now = Date.now()
+	
+	if (now > eventBuffering.flushTime) {
+		// Don't handle subsequent events until MAX_DELAY from now
+		eventBuffering.flushTime = now + EventBuffering.MAX_DELAY
+		handleEvents(newEvent)
+		return
+	}
+
+	eventBuffering.events.push(newEvent)
+	
+	if (eventBuffering.timeoutID === 0)  // If a timer hasn't already been set
+		eventBuffering.timeoutID = setTimeout(() => {
+			let bufferedEventsCopy
+			
+			/*synchronized(eventBuffering.events)*/
+			{
+				// This block must be synchronized. But not needed to be explicit in Node.js! See https://stackoverflow.com/a/9129149/5318303
+				bufferedEventsCopy = eventBuffering.events
+				eventBuffering.reset()
+			}
+			
+			handleEvents(...bufferedEventsCopy)
+		}, eventBuffering.flushTime - now /* => wait until flushTime */)
+}
+
 function handleEvents(...events) {
-	for (const event of events) console.log(event.event, event.fileName)
+	for (const event of events) console.log(event.event, event.path)
 	
 	refresh()
 }
