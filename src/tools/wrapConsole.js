3af3321e8a85481973469ea1ac636d11c8a458d0
Sean <sean@vigorwebsolutions.com>
2019-08-28 11:24:28 -0700

    Rewrite

--- src/tools/wrapConsole.js
+++ src/tools/wrapConsole.js
@@ -1,107 +1,75 @@
-/**
- * koji_utilities/wrapConsole.js
- * 
- * What it Does:
- *   This utility wraps around the normal console.log() function
- *   in order to allow koji to display the logs from your app in
- *   the embedded preview within your editor. 
- * 
- * What to Change:
- *   This file is fairly complicated and deals with internal koji
- *   schemas about how to handle iframe postMessage's, so changing
- *   around how this works is not advisable.
- * 
- * How to Use:
- *   Import this file and run it as a function once your component
- *   has mounted, see common/App.js to see an implementation. This
- *   is already done for you in this project so don't worry about it.
- */
-
-"use strict";
-
-module.exports = () => {
-  // Outgoing
-  window.__originalConsole = {
-    log: console.log.bind(console),
-    info: console.info.bind(console),
-    warn: console.warn.bind(console),
-    error: console.error.bind(console),
-  };
-
+const postToParent = ({ action, payload }) => {
   try {
-    const consoleOverrides = {
-      // ...console,
-      log: (...args) => {
-        window.__originalConsole.log.apply(this, args);
-        if (window.parent) {
-            try {
-              window.parent.postMessage({
-                  action: 'log',
-                  payload: { args },
-              }, '*');
-            } catch (err) {
-                //
-            }
-        }
-      },
-      info: (...args) => {
-        window.__originalConsole.info.apply(this, args);
-        if (window.parent) {
-          try {
-              window.parent.postMessage({
-                  action: 'info',
-                  payload: { args },
-              }, '*');
-            } catch (err) {
-                //
-            }
-        }
-      },
-      warn: (...args) => {
-        window.__originalConsole.warn.apply(this, args);
-        if (window.parent) {
-          try {
-              window.parent.postMessage({
-                  action: 'warn',
-                  payload: { args },
-              }, '*');
-            } catch (err) {
-                //
-            }
-        }
-      },
-      error: (...args) => {
-        window.__originalConsole.error.apply(this, args);
-        if (window.parent) {
-            try {
-              window.parent.postMessage({
-                  action: 'error',
-                  payload: { args },
-              }, '*');
-            } catch (err) {
-                //
-            }
-        }
-      },
-    };
-    console = consoleOverrides;
+    if (window && window.parent) {
+      window.parent.postMessage({
+        action,
+        payload,
+      }, '*');
+    }
   } catch (err) {
-    //
+    // err
   }
-
-  // Wrap error
-  window.onerror = (message, source, lineNumber) => {
-      if (window.parent) {
-          window.parent.postMessage({
-              action: 'error',
-              payload: {
-                  args: [
-                    message,
-                    `In file: ${source}`,
-                    `Line: ${lineNumber}`,
-                  ],
-              }
-          }, '*');
-      }
-  };
 };
+
+const wrapConsole = () => {
+  try {
+    if (window && window.console) {
+      // Log
+      const consoleLog = window.console.log;
+      window.console.log = (...args) => {
+        consoleLog.apply(this, args);
+        postToParent({
+          action: 'log',
+          payload: { args },
+        });
+      };
+
+      // Warn
+      const consoleWarn = window.console.warn;
+      window.console.warn = (...args) => {
+        consoleWarn.apply(this, args);
+        postToParent({
+          action: 'warn',
+          payload: { args },
+        });
+      };
+
+      // Info
+      const consoleInfo = window.console.info;
+      window.console.info = (...args) => {
+        consoleInfo.apply(this, args);
+        postToParent({
+          action: 'info',
+          payload: { args },
+        });
+      };
+
+      // Error
+      const consoleError = window.console.error;
+      window.console.error = (...args) => {
+        consoleError.apply(this, args);
+        postToParent({
+          action: 'error',
+          payload: { args },
+        });
+      };
+    }
+
+    window.onerror = (message, source, lineNumber) => {
+      postToParent({
+        action: 'error',
+        payload: {
+          args: [
+            message,
+            `In file: ${source}`,
+            `Line: ${lineNumber}`,
+          ],
+        },
+      });
+    };
+  } catch (err) {
+    // err
+  }
+};
+
+export default wrapConsole;
