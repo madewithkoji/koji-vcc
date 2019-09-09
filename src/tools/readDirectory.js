762ea4cac97fcbaa05f8ea198c411dc91d50313f
X-yn <ryftcore@gmail.com>
2019-09-10 05:22:15 +0530

    Removed infinite loop & added filter logic to readDirectory()

--- src/tools/readDirectory.js
+++ src/tools/readDirectory.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 const readDirectoryRelative = (directory) => {
   try {
-    let list = execSync('git ls-files && git ls-files --exclude-standard --others', { cwd: directory }).toString()
+    let pathList = execSync('git ls-files && git ls-files --exclude-standard --others', { cwd: directory }).toString()
       .replace(/\n$/, '')
       .split('\n');
 
@@ -12,30 +12,24 @@ const readDirectoryRelative = (directory) => {
     const submodulesInfo = execSync('git submodule status', { cwd: directory }).toString();
 
     const regExp = /^ [A-Fa-f0-9]{40,64} (.+?) \(.+?\)$/gm; // live: https://regex101.com/r/yUEJNe/2/
-
-    while (true) {
-      const match = regExp.exec(submodulesInfo);
-
-      if (match === null) break;
-
-      // const submoduleInfo = match[0]
+    const match = regExp.exec(submodulesInfo);
+    // const submoduleInfo = match[0]
+    while (match !== null) {
       const submodulePath = match[1];
-
       // Sample values for above variables:
       //     submoduleInfo: " debd72fe632d7315be8e31fe00c7e767c423a01f sub-repo1 (heads/master)"
       //     submodulePath: "sub-repo1"
 
       // Exclude submodule directory from the list:
-      list = list.filter((p) => p !== submodulePath);
-
+      pathList = pathList.filter((dirPath) => dirPath !== submodulePath);
       // Instead, add paths under the submodule (recursive):
       // Make path relative to `directory`:
-      list.push(...readDirectoryRelative(path.resolve(directory, submodulePath)).map(
-        (nestedPath) => path.join(submodulePath, nestedPath),
-      ));
-    }
+      const relativeDirPaths = readDirectoryRelative(path.resolve(directory, submodulePath))
+        .map((nestedPath) => path.join(submodulePath, nestedPath));
 
-    return list;
+      pathList.push(...relativeDirPaths);
+    }
+    return pathList;
   } catch (err) {
     const error = new Error(`
       [@withkoji/vcc] ${err.message}\n${_YLW}${RED}Have you installed "git" and added it to the "PATH"?
@@ -48,8 +42,15 @@ const readDirectoryRelative = (directory) => {
 
 // Get all git-indexed paths to find koji files
 // Make path absolute:
-const readDirectory = (directory) => readDirectoryRelative(directory).map(
-  (relativePath) => path.resolve(directory, relativePath),
-);
+// Filter out any files that dont meet requirements
+const readDirectory = (directory) => readDirectoryRelative(directory)
+  .reduce((absoluteDirs, relativePath) => {
+    if (!((relativePath.endsWith('koji.json') || relativePath.includes('.koji')) && !relativePath.includes('.koji-resources'))) {
+      return absoluteDirs;
+    }
+    const absolutePath = path.resolve(directory, relativePath);
+    absoluteDirs.push(absolutePath);
+    return absoluteDirs;
+  }, []);
 
 export default readDirectory;
