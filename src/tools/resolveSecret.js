d79ea4e748ea5c4acbc2e9d993fef42cc5cf2d0a
X-yn <ryftcore@gmail.com>
2019-09-10 05:50:32 +0530

    added error-handling and removed the if condition

--- src/tools/resolveSecret.js
+++ src/tools/resolveSecret.js
@@ -1,14 +1,14 @@
 const resolveSecret = (key) => {
-  if (!process || !process.env || !process.env.KOJI_SECRETS) {
-    return null;
-  }
   try {
     const parsedSecrets = JSON.parse(process.env.KOJI_SECRETS);
     return parsedSecrets[key] || null;
   } catch (err) {
-    //
+    const error = new Error(
+      `[@withkoji/vcc] ${err.message}\nPlease verify that you've defined an environment variable named KOJI_SECRETS`,
+    );
+    error.stack = err.stack;
+    throw err;
   }
-  return null;
 };
 
 export default resolveSecret;
