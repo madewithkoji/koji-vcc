33eb5670ecd272302ca89c0828d9b13ba61707c2
sean <admin@koji-projects.com>
2019-10-08 11:32:11 -0700

    Update to watch entire dir

--- src/watch.js
+++ src/watch.js
@@ -1,6 +1,5 @@
 import chokidar from 'chokidar';
 
-import readDirectory from './tools/readDirectory';
 import findRootDirectory from './tools/findRootDirectory';
 
 import writeConfig from './tools/writeConfig';
@@ -9,22 +8,18 @@ const watch = () => {
   // Generate a base config
   writeConfig();
 
-  // Watch the .koji directory from a node_modules directory
-  const files = readDirectory(findRootDirectory())
-    .filter((path) => (path.endsWith('koji.json') || path.includes('.koji')) && !path.includes('.koji-resources'));
-
   // Note: Polling is used by default in the container via
   // the CHOKIDAR_USEPOLLING=1 env that is set in the container
-  const watcher = chokidar.watch(files);
+  const rootDir = findRootDirectory();
+  const watcher = chokidar.watch(rootDir);
 
   watcher
     .on('error', (error) => console.error(`[@withkoji/vcc] Watcher error: ${error}`))
-    .on('change', () => {
+    .on('all', () => {
       writeConfig();
     })
     .on('ready', () => {
-      const watched = watcher.getWatched();
-      Object.keys(watched).map((path) => watched[path].map((file) => console.log(`[@withkoji/vcc] Watching ${path}/${file}`)));
+      console.log(`[@withkoji/vcc] Watching ${rootDir}...`);
     });
 };
 
