66bfd46c5096a61573de683836862b8e13c32637
sean <admin@koji-projects.com>
2019-10-11 14:08:01 -0700

    Debounce watcher

--- src/watch.js
+++ src/watch.js
@@ -13,11 +13,14 @@ const watch = () => {
   const kojiDir = `${findRootDirectory()}/.koji`;
   const watcher = chokidar.watch(kojiDir);
 
+  // eslint-disable-next-line no-unused-vars
+  let watcherDebounce = null;
+
   watcher
     .on('error', (error) => console.error(`[@withkoji/vcc] Watcher error: ${error}`))
     .on('all', () => {
       console.log('[@withkoji/vcc] Rebuilding config...');
-      writeConfig();
+      watcherDebounce = setTimeout(() => writeConfig(), 250);
     })
     .on('ready', () => {
       console.log(`[@withkoji/vcc] Watching ${kojiDir}...`);
