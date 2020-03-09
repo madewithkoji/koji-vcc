2b85281e7bfc7a31d6c54fb417281cf3f1382655
bdjeffyp <bdjeffyp@noreply.users.github.com>
2020-03-09 14:08:11 -0700

    Resolve no default Config export

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,9 @@
-// Type definitions for koji-vcc 1.1.2
+// Type definitions for koji-vcc 1.1.4
 // Project: https://github.com/madewithkoji/koji-vcc
 // Definitions by: Jeff Peterson <https://github.com/bdjeffyp>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 3.7
-import Config from './res/config.json';
+import { Config } from './res/config.json';
 
 declare module "@withkoji/vcc" {
   class Handle {
