e2d73759cae7d76fb8f59a640ff3154c8682ecdd
Sean Thielen <admin@koji-projects.com>
2020-12-09 12:43:04 -0800

    Signed URL tool for koji-cdn objects

--- src/Keystore/index.js
+++ src/Keystore/index.js
@@ -30,6 +30,39 @@ export default class Keystore {
     return decryptedValue;
   }
 
+  // For objects stored on koji CDN, you can generate a signed URL to serve
+  // them without exposing a permanent URI. End users can obviously still
+  // download and rehost the content, or screenshot it/etc (anyone sufficiently
+  // dedicated can rip anything...), but this provides an easy way to filter out
+  // lazy attackers.
+  //
+  // Pass in the full URL of a resource to return a url that
+  // looks like `https://objects.koji-cdn.com/signed/TOKEN` (note that HLS video
+  // URLs will look different than this). If expirySeconds is left undefined,
+  // signed videos expire after 1 hour, and any other resource expires after 5
+  // minutes. If you wish to apply querystring parameters to use the image
+  // api (?width=, etc.), apply those parameters to the signed URL, not to the
+  // resource argument.
+  generateSignedUrl(resource, expirySeconds) {
+    const request = await fetch(
+      this.buildUri('/v1/cdn/signedRequest/create'),
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Koji-Project-Id': this.projectId,
+          'X-Koji-Project-Token': this.projectToken
+        },
+        body: JSON.stringify({
+          resource,
+          expirySeconds,
+        }),
+      },
+    );
+    const { url } = await request.json();
+    return url;
+  }
+
   buildUri(path) {
     if (process.env.NODE_TEST) {
       return `http://localhost:3129${path}`;
